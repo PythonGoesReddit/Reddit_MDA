0c924b544711ec2fab55aba8e4bce152347768fa
HannaMahler <68068078+HannaMahler@users.noreply.github.com>
2020-10-03 12:15:57 +0200

    Worked on grouping of features

--- Hannas_interim_test_script.py
+++ Hannas_interim_test_script.py
@@ -71,11 +71,11 @@ def feature_04(untagged_list):
 ### 1. Create a dictionary with an several entries that each have an
 ###    element called "body" containing some text
 
-dict1 = {"text1": {"body": "Is this really all that you remember from your Python class, which really wasn't that long ago?"}, 
+dict1 = {"text1": {"body": "Is this really all that you remember from your Python class, which really was not that long ago?"}, 
          "text2": {"body": "Well, my dear, I think you should better get off your desk now and take a well-deserved break outside!"}, 
          "text3": {"body": "There are a number of things to pay attention to when composing this intricate piece of code - your snoring dog is not one of them."}, 
          "text4": {"body": "Apart from eating ice cream, every team member musn't forget to always push their changes to the GitHub page."}, 
-         "text5": {"body": "I feel like in the end all of this is going to take much longer than anyone of us has hoped for."}}
+         "text5": {"body": "The dog that I saw isn't going to take slightly longer than I'd hoped for."}}
 
 ### 2. Open each dictionary entry in the three formats that I need
 for text in dict1:
--- Reddit_Bibers_Features.py
+++ Reddit_Bibers_Features.py
@@ -36,16 +36,20 @@ import string
 ## - feature 55: public verbs
 ## - feature 56: private verbs
 ## - feature 57: suasive verbs
+## - feature 23: WH-clauses (depend on public/private/suasive verbs)
+## - feature 60: THAT deletion (depends on public/private/suasive verbs)
   ## PRIMARY VERBS ONLY
 ## - feature 12: 'do' as a pro-verb
 ## - feature 19: 'be' as main verb
 ## - feature 17: agentless passives
 ## - feature 18: 'by' passives
+## - feature 59: contractions
   ## MODAL AUXILIARIES ONLY
 ## - feature 52: possibility modals
 ## - feature 53: necessity modals
 ## - feature 54: predictive modals
 ## - feature 63: split auxiliaries
+## - feature 59: contractions
 
 ## ADVERBS:
 ## - feature 04: place adverbials
@@ -56,6 +60,8 @@ import string
 ## - feature 38: adv. subordinator, other
 ## - feature 42: total adverbs
 ## - feature 48: amplifiers
+## - feature 67: analytic negation
+## - feature 46: downtoners
 
 ## ADJECTIVES
 ## - feature 40: attributive adjective
@@ -79,35 +85,40 @@ import string
 ## - feature 09: pronoun 'it'
 ## - feature 10: demonstrative pronouns
 ## - feature 11: indefinite pronouns
+## - feature 59: contractions
+  
+## CONJUNCTIONS
+## - feature 64: phrasal coordination
+## - feature 65: non-phrasal coordination
+
+## DETERMINERS:
+## - feature 66: synthetic negation  
+## - feature 51: demonstratives
+  
+## WH-stuff (basically all tags starting with W)
+## - feature 34: sentence relatives ('which')
+## - feature 31: WH relatives, subject position
+## - feature 32: WH relatives, object position
+## - feature 33: WH relatives, pied piping
+## - feature 13: WH-questions
 
 ## FOR THE WHOLE SENTENCE
 ## - feature 43: type/token ratio
 ## - feature 44: word length
 
 ## STILL UNCLEAR:
-## - feature 13: WH-questions
 ## - feature 20: existential 'there' -> what would 'there' be tagged as?
 ## - feature 21: 'that' verb complements -> what would 'that' be tagged as?
 ## - feature 22: 'that' adjective complements -> what would 'that' be tagged as?
-## - feature 23: WH-clauses
 ## - feature 29: 'that' relatives, subject position
 ## - feature 30: 'that' relatives, object position
-## - feature 31: WH relatives, subject position
-## - feature 32: WH relatives, object position
-## - feature 33: WH relatives, pied piping
-## - feature 34: sentence relatives ('which')
 ## - feature 45: conjuncts (belong to multiple word classes)
-## - feature 46: downtoners (these would probably all be tagged as adverbs?)
 ## - feature 47: hedges (belong to multiple word classes)
 ## - feature 49: emphatics (belong to multiple word classes)
 ## - feature 50: discourse particles -> what would they be tagged as?
-## - feature 51: demonstratives -> what would they be tagged as?
-## - feature 59: contractions -> what would they be tagged as?
-## - feature 60: THAT deletion (depends on multiple word classes)
-## - feature 64: phrasal coordination -> what is 'and' tagged as?
-## - feature 65: non-phrasal coordination (depends on multiple word classes)
-## - feature 66: synthetic negation
-## - feature 67: analytic negation
+
+
+
 
 
 
