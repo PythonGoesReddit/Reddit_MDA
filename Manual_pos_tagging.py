b2e870ec66dd900d90afb299dbfe0e316c041326
axelrelaxel <axel@bohmann.de>
2021-09-29 15:19:29 +0200

    Changed the folder name for Gustavo's tagged data to get rid of whitespace; updated the Python script to extract sentences for manual POS-tagging

--- Manual_pos_tagging.py
+++ Manual_pos_tagging.py
@@ -6,33 +6,62 @@ Created on Wed Sep 29 07:59:53 2021
 """
 
 import json
-import nltk
 
-file = "/Users/mahle/Documents/Uni/01_Master/Unterlagen/Python/Python_group/RC_2005-12_tagged_FLAIR.json"
+filepath = "Tagged_JSONS/RC_2005-12_tagged_FLAIR.json"
 
-p = open("/Users/mahle/Documents/Uni/01_Master/Unterlagen/Python/Python_group/RC_2005-12_tagged_manual.txt", "w")
 counter = 1
 
-with open(file) as f:
-    
-    for item in file:
-        counter +=1
-        
-        if counter > 0 and counter < 101: ## specify range here
+with open(filepath) as f:
+    contents = f.read().strip("[]") # The stripping gets rid of irregular square brackets in the input file
+    sentences = contents.replace("}, {","}\n{").split("\n") # The replace and split makes sure separate entries are handled separately (input file has no line breaks)
+    p = open("Tagged_JSONS/RC_2005-12_tagged_manual_Batch1.txt", "w")
+    for x in range(0,100):
+        data = json.loads(sentences[x])
+        tagged = data["sentence_tagged"][0] # the index 0 just gets rid of one superfluous level of parentheses
+        untagged = [[x[0],"_"] for x in tagged]
+        link_id = data["link_id"]
+        sentence_no = data["sentence_no"]
+        p.write("\t".join(lin_id, sentence_no, untagged)+"\n")
+    p.close()
+    p = open("Tagged_JSONS/RC_2005-12_tagged_manual_Batch2.txt", "w")
+    for x in range(100,200):
+        data = json.loads(sentences[x])
+        tagged = data["sentence_tagged"][0] # the index 0 just gets rid of one superfluous level of parentheses
+        untagged = [[x[0],"_"] for x in tagged]
+        link_id = data["link_id"]
+        sentence_no = data["sentence_no"]
+        p.write("\t".join(lin_id, sentence_no, untagged)+"\n")
+    p.close()
+    p = open("Tagged_JSONS/RC_2005-12_tagged_manual_Batch3.txt", "w")
+    for x in range(200,300):
+        data = json.loads(sentences[x])
+        tagged = data["sentence_tagged"][0] # the index 0 just gets rid of one superfluous level of parentheses
+        untagged = [[x[0],"_"] for x in tagged]
+        link_id = data["link_id"]
+        sentence_no = data["sentence_no"]
+        p.write("\t".join(lin_id, sentence_no, untagged)+"\n")
+    p.close()       
+    p = open("Tagged_JSONS/RC_2005-12_tagged_manual_Batch4.txt", "w")
+    for x in range(300,400):
+        data = json.loads(sentences[x])
+        tagged = data["sentence_tagged"][0] # the index 0 just gets rid of one superfluous level of parentheses
+        untagged = [[x[0],"_"] for x in tagged]
+        link_id = data["link_id"]
+        sentence_no = data["sentence_no"]
+        p.write("\t".join(lin_id, sentence_no, untagged)+"\n")
+    p.close()
+    p = open("Tagged_JSONS/RC_2005-12_tagged_manual_Batch5.txt", "w")
+    for x in range(400,500):
+        data = json.loads(sentences[x])
+        tagged = data["sentence_tagged"][0] # the index 0 just gets rid of one superfluous level of parentheses
+        untagged = [[x[0],"_"] for x in tagged]
+        link_id = data["link_id"]
+        sentence_no = data["sentence_no"]
+        p.write("\t".join(lin_id, sentence_no, untagged)+"\n")
+    p.close()       
+                  
             
-            sentence = json.loads(item.strip())["body"]
-            link_id = json.loads(item.strip())["link_id"]
-            sentence_no = json.loads(item.strip())["sentence_no"]
-            
-            tokens = nltk.word_tokenize(sentence)
-            tags_long = ""
-            
-            for token in tokens:
-                tags = '["' + str(token) + ', ""], '
-                tags_long.append(tags)
-                p.write('{link_id: "' + str(link_id) + '", "sentence_no": ' + str(sentence_no) + ', manual_tagging: [[' + str(tags_long) + ']]}\n')
-        
-p.close()
+
 
 
 
