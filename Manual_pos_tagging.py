d801abd0a9c7a7adf880718a7900cb1eab602a5b
axelrelaxel <axel@bohmann.de>
2021-09-29 17:17:17 +0200

    Cleaner version of the untagged-sentence-extraction script plus results files.

--- Manual_pos_tagging.py
+++ Manual_pos_tagging.py
@@ -9,57 +9,21 @@ import json
 
 filepath = "Tagged_JSONS/RC_2005-12_tagged_FLAIR.json"
 
-counter = 1
-
 with open(filepath) as f:
     contents = f.read().strip("[]") # The stripping gets rid of irregular square brackets in the input file
     sentences = contents.replace("}, {","}\n{").split("\n") # The replace and split makes sure separate entries are handled separately (input file has no line breaks)
-    p = open("Tagged_JSONS/RC_2005-12_tagged_manual_Batch1.txt", "w")
-    for x in range(0,100):
+    batch = 1
+    for x in range(len(sentences)):
+        if x//100 == batch: # with each new multiple of 100 reached, increase the batch variable by 1
+            batch +=1
         data = json.loads(sentences[x])
         tagged = data["sentence_tagged"][0] # the index 0 just gets rid of one superfluous level of parentheses
-        untagged = [[x[0],"_"] for x in tagged]
+        untagged = [[x[0],"_"] for x in tagged][3:-3] # the [3:-3] slice gets rid of the three "X"s at the beginning and end of each sentence.
         link_id = data["link_id"]
         sentence_no = data["sentence_no"]
-        p.write(link_id + "	" + str(sentence_no) + "	" + str(untagged) +"\n")
-    p.close()
-    p = open("Tagged_JSONS/RC_2005-12_tagged_manual_Batch2.txt", "w")
-    for x in range(100,200):
-        data = json.loads(sentences[x])
-        tagged = data["sentence_tagged"][0] # the index 0 just gets rid of one superfluous level of parentheses
-        untagged = [[x[0],"_"] for x in tagged]
-        link_id = data["link_id"]
-        sentence_no = data["sentence_no"]
-        p.write(link_id + "\t" + str(sentence_no) + "\t" + str(untagged) +"\n")
-    p.close()
-    p = open("Tagged_JSONS/RC_2005-12_tagged_manual_Batch3.txt", "w")
-    for x in range(200,300):
-        data = json.loads(sentences[x])
-        tagged = data["sentence_tagged"][0] # the index 0 just gets rid of one superfluous level of parentheses
-        untagged = [[x[0],"_"] for x in tagged]
-        link_id = data["link_id"]
-        sentence_no = data["sentence_no"]
-        p.write(link_id + "\t" + str(sentence_no) + "\t" + str(untagged) +"\n")
-    p.close()       
-    p = open("Tagged_JSONS/RC_2005-12_tagged_manual_Batch4.txt", "w")
-    for x in range(300,400):
-        data = json.loads(sentences[x])
-        tagged = data["sentence_tagged"][0] # the index 0 just gets rid of one superfluous level of parentheses
-        untagged = [[x[0],"_"] for x in tagged]
-        link_id = data["link_id"]
-        sentence_no = data["sentence_no"]
-        p.write(link_id + "\t" + str(sentence_no) + "\t" + str(untagged) +"\n")
-    p.close()
-    p = open("Tagged_JSONS/RC_2005-12_tagged_manual_Batch5.txt", "w")
-    for x in range(400,500):
-        data = json.loads(sentences[x])
-        tagged = data["sentence_tagged"][0] # the index 0 just gets rid of one superfluous level of parentheses
-        untagged = [[x[0],"_"] for x in tagged]
-        link_id = data["link_id"]
-        sentence_no = data["sentence_no"]
-        p.write(link_id + "\t" + str(sentence_no) + "\t" + str(untagged) +"\n")
-    p.close()       
-                  
+        with open("Tagged_JSONS/RC_2005-12_tagged_manual_Batch" + str(batch) + ".txt", "a") as p:
+            p.write(link_id + "	" + str(sentence_no) + "	" + str(untagged) +"\n")
+    p.close()   
             
 
 
