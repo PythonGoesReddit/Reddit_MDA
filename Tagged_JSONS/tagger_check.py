717f7eef8f829498ffdde191a496a91dbb6026d3
kyla-mcconnell <kylamcconnell@live.com>
2021-12-06 11:45:13 +0100

    Create tagger_check.py

---
+++ Tagged_JSONS/tagger_check.py
@@ -0,0 +1,6 @@
+# Read in data from NLTK and from Flair, retrieve ID and tagged sentence
+# Drop additional [X,X] items
+# Read in manual POS tags 
+# Match on ID
+# Check accuracy 
+# Output mismatched items (total tags, correct tags -- human is correct)
