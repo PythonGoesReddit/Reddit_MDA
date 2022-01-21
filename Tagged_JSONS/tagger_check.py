2412f70b1291f93cd1638b360670c60481e9cd6a
kyla-mcconnell <kylamcconnell@live.com>
2022-01-21 11:52:38 +0100

    Add planning notes to tagger_check, update json file formats

--- Tagged_JSONS/tagger_check.py
+++ Tagged_JSONS/tagger_check.py
@@ -28,3 +28,13 @@ with open(os.path.join(dirname, 'RC_2005-12_tagged_FLAIR.json'), "r", errors="re
 # combining the individual ranking functions
 # json files currently lists 
 # matching based on the human coding identifiers (not currently written as a dict)
+
+#read in line, split at backslash t, pull out elements indexed 1, 2, 3
+#take element 3 of the human-coded items, which will be a string, and turn this into a list of tags only; make the machine-coded the same format
+#make function
+#x = x.strip("[]")
+#list1 = x.split("], [")
+#list2 = [x.strip("'") for x in list1]
+#list3 = [y.split("', '") for y in list2]
+#check how many times list 1 at index x is not the same as list 2 at index x
+#write to notes file if not
